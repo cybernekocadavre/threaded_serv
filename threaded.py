bff9656d4e2f351d579a24b495ef5d83001b0fbd
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 15:52:37 +0300

    Add files via upload

---
+++ threaded.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+import threading
+
+def echo_server(client_socket, client_address):
+    print(f"Connected to {client_address}")
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        client_socket.sendall(data)
+    print(f"Disconnected from {client_address}")
+    client_socket.close()
+
+def handle_client_connections(server_socket):
+    
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_thread = threading.Thread(target=echo_server, args=(client_socket, client_address))
+        client_thread.start()
+
+def main():
+    server_host = '127.0.0.1'
+    server_port = 12345
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_host, server_port))
+    server_socket.listen(5)
+    print(f"Server listening on {server_host}:{server_port}")
+
+    client_handler_thread = threading.Thread(target=handle_client_connections, args=(server_socket,))
+    client_handler_thread.start()
+
+if __name__ == "__main__":
+    main()
+
