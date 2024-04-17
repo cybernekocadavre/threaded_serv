78a87a1b57a446ad0b6983caaf7174cb7f16c5cf
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 17:24:39 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,10 +3,12 @@
 
 # In[ ]:
 
+# client.py
+
 import socket
 import sys
 
-def echo_client(message):
+def echo_client():
     server_host = '127.0.0.1'
     server_port = 12345
 
@@ -16,23 +18,25 @@ def echo_client(message):
     # Connect to the server
     client_socket.connect((server_host, server_port))
 
-    # Send the message to the server
-    client_socket.sendall(message.encode())
+    # Receive username prompt from the server
+    username_prompt = client_socket.recv(1024).decode()
+    print(username_prompt, end="")
+    
+    # Input username and send to server
+    username = input()
+    client_socket.sendall(username.encode())
 
-    # Receive data from the server
-    data = client_socket.recv(1024)
-    print("Received:", data.decode())
+    # Continuous sending of messages
+    while True:
+        message = input("Enter your message: ")
+        client_socket.sendall(message.encode())
+
+        # Receive and print data from the server
+        data = client_socket.recv(1024)
+        print("Received:", data.decode())
 
     # Close the socket
     client_socket.close()
 
 if __name__ == "__main__":
-    # Check if a message is provided as a command line argument
-    if len(sys.argv) != 2:
-        print("Usage: python client.py <message>")
-        sys.exit(1)
-
-    message = sys.argv[1]
-    echo_client(message)
-
-
+    echo_client()
