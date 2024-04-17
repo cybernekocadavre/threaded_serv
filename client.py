d36be8c3dd3ee5d81016d21ed07e719109f80682
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 16:59:50 +0300

    Update client.py

--- client.py
+++ client.py
@@ -18,36 +18,28 @@ def chat_client():
         data = client_socket.recv(1024)
         print(data.decode(), end='')
         username = input()
-    
+
         # Send username to the server
         client_socket.sendall(username.encode())
-    
+
         while True:
             # Prompt user for message
             message = input("Enter your message: ")
             if not message:
                 break  # Exit loop if message is empty
-    
+
             # Send message to the server
             client_socket.sendall(message.encode())
-    
+            
             # Receive and print messages from the server
-            while True:
-                data = client_socket.recv(1024)
-                if not data:
-                    break  # Exit inner loop if no more data
-                print("Received:", data.decode(), end='')  # Print received message without newline
-    
-            # Prompt user for a new message
-            print("Enter your message: ", end='', flush=True)  # Flush the buffer to ensure prompt is shown immediately
+            data = client_socket.recv(1024)
+            print("Received:", data.decode(), end='')  # Print received message without newline
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
         # Close the connection
         client_socket.close()
 
-
 if __name__ == "__main__":
     chat_client()
-
 
