77c45262144ddfd7812a3d5527f0e57b2e12df57
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 17:33:08 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -6,22 +6,19 @@
 import socket
 
 def port_scanner(host):
-    print(f"Scanning ports on {host}...")
-    for port in range(1, 65536):
+    print(f"Проверяем порты {host}...")
+    for port in range(1, 100):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(0.1)
                 result = s.connect_ex((host, port))
                 if result == 0:
-                    print(f"Port {port} is open")
+                    print(f"Порт {port} открыт")
                 s.close()
-        except KeyboardInterrupt:
-            print("\nScan interrupted by user.")
-            break
         except socket.error:
-            print("Couldn't connect to server.")
+            print("Подключение к серверу невозможно.")
             break
 
 if __name__ == "__main__":
-    host = input("Enter the hostname or IP address to scan: ")
+    host = input("Введите IP или имя хоста: ")
     port_scanner(host)
