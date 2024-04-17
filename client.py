53457ac78ecb660ff3e9b6c82d5b77fb9af20a61
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 16:31:35 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@
 import socket
 import sys
 
-def echo_client(message):
+def chat_client(username, message):
     server_host = '127.0.0.1'
     server_port = 12345
 
@@ -15,19 +15,30 @@ def echo_client(message):
 
     client_socket.connect((server_host, server_port))
 
+    # Send username to the server
+    client_socket.sendall(username.encode())
+
+    # Receive username prompt from the server
+    data = client_socket.recv(1024)
+    print(data.decode())
+
+    # Send message to the server
     client_socket.sendall(message.encode())
 
+    # Receive and print response from the server
     data = client_socket.recv(1024)
     print("Received:", data.decode())
 
     client_socket.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python client.py <message>")
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <username> <message>")
         sys.exit(1)
 
-    message = sys.argv[1]
-    echo_client(message)
+    username = sys.argv[1]
+    message = sys.argv[2]
+    chat_client(username, message)
+
 
 
