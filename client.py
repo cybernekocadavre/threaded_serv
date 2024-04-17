0e3e07b324398dde7f00a78ff1054d7403ea7740
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-17 17:11:14 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,53 +3,30 @@
 
 # In[ ]:
 
-
 import socket
-import threading
+import sys
 
-def receive_messages(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        print(data.decode(), end='')
-
-def chat_client():
+def echo_client(message):
     server_host = '127.0.0.1'
     server_port = 12345
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     client_socket.connect((server_host, server_port))
 
-    try:
-        # Receive prompt for username
-        data = client_socket.recv(1024)
-        print(data.decode(), end='')
-        username = input()
+    client_socket.sendall(message.encode())
 
-        # Send username to the server
-        client_socket.sendall(username.encode())
+    data = client_socket.recv(1024)
+    print("Received:", data.decode())
 
-        # Start a thread to continuously receive messages from the server
-        receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-        receive_thread.start()
-
-        while True:
-            # Prompt user for message
-            message = input("Enter your message: ")
-            if not message:
-                break  # Exit loop if message is empty
-
-            # Send message to the server
-            client_socket.sendall(message.encode())
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        # Close the connection
-        client_socket.close()
+    client_socket.close()
 
 if __name__ == "__main__":
-    chat_client()
+    if len(sys.argv) != 2:
+        print("Usage: python client.py <message>")
+        sys.exit(1)
 
+    message = sys.argv[1]
+    echo_client(message)
 
 
